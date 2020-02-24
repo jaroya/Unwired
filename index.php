207fd0aa736ce8b6db07c1b986f9f294dc29ace9
Mica Jaroya <michael.jaroya@gmail.com>
2020-02-24 04:30:29 +0300

    Revert "Revert "God is good p"" yes
    This reverts commit e1cd7d71afe9a93677d0f5b7d209cf1db4b0d83f.

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 
 	<div>
 		<p>XSS stands for Cross-site scripting</p>
+		<h4>Good old songs make my day</h4>
+		<ul>
+			<li>God is Good</li>
+			<li>All the time</li>
+		</ul>
 	</div>
 
 </body>
