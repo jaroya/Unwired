e1cd7d71afe9a93677d0f5b7d209cf1db4b0d83f
Mica Jaroya <michael.jaroya@gmail.com>
2020-02-24 04:25:44 +0300

    Revert "God is good p" I remembered to delete it This reverts commit ce9c787944e6582522d9e1ae714fcf85a4932537.

--- index.php
+++ index.php
@@ -8,11 +8,6 @@
 
 	<div>
 		<p>XSS stands for Cross-site scripting</p>
-		<h4>Good old songs make my day</h4>
-		<ul>
-			<li>God is Good</li>
-			<li>All the time</li>
-		</ul>
 	</div>
 
 </body>
